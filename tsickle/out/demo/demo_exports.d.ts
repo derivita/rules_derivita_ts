c030c9f230f791b3c73771f547a166e6c242d97a
Ryan Brown <ryan@derivita.com>
2025-06-02 08:46:07 -0600

    Remove tsickle compilation output.

--- tsickle/out/demo/demo_exports.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function foo(): string;
-export declare function bar(): string;
-export type SpecialType = string | number;
-export declare function baz(): SpecialType;
-export declare class MyClass {
-    private objName;
-    constructor(objName: string);
-    getName(): string;
-}
--- tsickle/out/demo/demo_star_exports.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./demo_exports";
-export * as decorators from "./decorators";
